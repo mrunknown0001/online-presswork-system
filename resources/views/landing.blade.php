1102ddc485a0ad1100846872eb40ff7d428daa5b
Michael Adam <madamt0001@gmail.com>
2018-10-16 22:25:08 +0800

    UPdate

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -6,22 +6,12 @@
 <div class="header">
      <div class="container">
         <div class="row">
-           <div class="col-md-10">
+           <div class="col-md-12">
               <div class="logo">
-                 <h1><a>Online Presswork System for The Work at Tarlac State University</a></h1>
+                 <h1 class="text-center"><a>Online Presswork System for The Work at Tarlac State University</a></h1>
               </div>
            </div>
-           <div class="col-md-2">
-            <div class="navbar navbar-inverse" role="banner">
-                <nav class="collapse navbar-collapse bs-navbar-collapse navbar-right" role="navigation">
-                  <ul class="nav navbar-nav">
-                    <li>
-                      <a href="{{ route('login') }}"><span class="glyphicon glyphicon-log-in"></span> Login</b></a>
-                    </li>
-                  </ul>
-                </nav>
-            </div>
-           </div>
+
         </div>
      </div>
 </div>
