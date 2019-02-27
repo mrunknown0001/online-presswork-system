0a1a94e8ebb7a5638394d830007da7aab2580a44
Michael Adam <madamt0001@gmail.com>
2019-02-28 01:43:40 +0800

    Update

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -8,6 +8,36 @@
 		<h3>Dashboard</h3>
 
 		@include('includes.all')
+
+		<div class="row">
+			<div class="col-md-4">
+			 <div class="card">
+		      <div class="card-body">
+		        <h3 class="card-title text-center"></h3>
+		        <p class="card-text text-center">Approved Layouts</p>
+		        <a href="{{ route('admin.publish') }}" class="btn btn-primary btn-block">Layouts</a>
+		      </div>
+		    </div>
+			</div>
+			<div class="col-md-4">
+			  <div class="card">
+		      <div class="card-body">
+		        <h3 class="card-title text-center"></h3>
+		        <p class="card-text text-center">Activity Logs</p>
+		        <a href="{{ route('admin.activity.log') }}" class="btn btn-primary btn-block">Activity Logs</a>
+		      </div>
+		    </div>
+			</div>
+			<div class="col-md-4">
+			  <div class="card">
+		      <div class="card-body">
+		        <h3 class="card-title text-center"></h3>
+		        <p class="card-text text-center">Database Backup and Restore</p>
+		        <a href="{{ route('admin.backup.database') }}" class="btn btn-primary btn-block">Backup and Restore</a>
+		      </div>
+		    </div>
+			</div>
+		</div>
 	</div>
 	
 </div>
--- resources/views/se/dashboard.blade.php
+++ resources/views/se/dashboard.blade.php
@@ -8,6 +8,31 @@
 		<h3>Dashboard</h3>
 
 		@include('includes.all')
+
+		<div class="row">
+			<div class="col-md-4">
+				 <div class="card">
+			      <div class="card-body">
+			        <h3 class="card-title text-center"></h3>
+			        <p class="card-text text-center">Approved Articles</p>
+			        <a href="{{ route('se.approved.articles') }}" class="btn btn-primary btn-block">View Articles</a>
+			      </div>
+				</div>
+			</div>
+			<div class="col-md-4">
+		      <div class="card-body">
+		        <h3 class="card-title text-center"></h3>
+		        <p class="card-text text-center">Denied Articles</p>
+		        <a href="{{ route('se.view.denied.article') }}" class="btn btn-primary btn-block">View Articles</a>
+		      </div>
+			</div>
+			<div class="col-md-4">
+		      <div class="card-body">
+		        <h3 class="card-title text-center"></h3>
+		        <p class="card-text text-center">Proofreaded Articles</p>
+		        <a href="{{ route('se.article.proofreaded') }}" class="btn btn-primary btn-block">View Articles</a>
+		      </div>
+			</div>
 	</div>
 </div>
 @endsection
